8570c5a95245cc7f1726fb3da98ec67fc1669bd3
scarlettperry <si.perry15@gmail.com>
2018-07-20 23:04:58 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    if self.size == 0
+      0
+    elsif self.size != 0
+      self.split(/[.?!]+/).grep(/\S/).count
+    end
   end
 end
